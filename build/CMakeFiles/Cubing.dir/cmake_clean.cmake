ed7cc9809cdaae215c5735d15679dc735b14bbd2
Elijah Villarmia <ej.villarmia@gmail.com>
2023-01-11 13:21:55 +0000

    First commit

---
+++ build/CMakeFiles/Cubing.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cubing.dir/main.cpp.obj"
+  "CMakeFiles/Cubing.dir/main.cpp.obj.d"
+  "Cubing.exe"
+  "Cubing.exe.manifest"
+  "Cubing.pdb"
+  "libCubing.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cubing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
